e988479d4969d3d6f66d3a3f1578bb44af954ddb
Garpit99 <wisjijamak@gmail.com>
2026-03-24 00:43:22 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const { connect } = require('./db/database');
-const bot = require('./index'); // ✅ FIX PATH
+const bot = require('./bot/index'); // ✅ FIX PATH
 
 const app = express();
 const PORT = process.env.PORT || 3000;
